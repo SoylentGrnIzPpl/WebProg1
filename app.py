60499b57e40c07bcc5f0715f0335b810c1367e86
Patrick <pdr721@gmail.com>
2022-06-14 21:56:13 -0400

    messing with template injection

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def get_index():
-    return "<p>Hello from flask!</p>"
+    return "<p>Hello from flask :)</p>"
+
+@app.route("/hello")
+def get_hello():
+    return render_template('hello.html', name="Santa")
