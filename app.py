24fa9089833fca089a0be818791d972ea8e4e696
Patrick <pdr721@gmail.com>
2022-06-16 21:35:03 -0400

    working with forms

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
 
@@ -10,3 +13,22 @@ def get_index():
 @app.route("/hello")
 def get_hello():
     return render_template('hello.html', name="Santa")
+
+@app.route("/hi")
+@app.route("/hi/<name>")
+def get_hi(name="Dick"):
+    name = name[::-1]
+    return render_template('hello.html', name=name)
+
+@app.route("/login", methods=['GET'])
+def get_login():
+    return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def post_login():
+    username = request.form.get("username", "<missing username>")
+    password = request.form.get("password", "<missing password>")
+    if password == "pass":
+        return redirect(url_for('get_hi', name=username))
+    else:
+        return redirect(url_for('get_login'))
